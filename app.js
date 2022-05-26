39c1a1648ccdec2df929bf54e1a0c7a094245572
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-26 09:47:32 -0300

    commit changes

--- app.js
+++ app.js
@@ -7,6 +7,32 @@ const path = require('path');
 
 const basePath = path.join(__dirname, 'template')
 
+const checkAuth = function(req, res, next){
+
+    req.authStatus = true;
+
+
+    if(req.authStatus){
+
+        next()
+        console.log("Está logado no sistema!!!!, pode continuar a usar")
+
+    }else{
+
+        console.log("Não Está logado no sistema ainda !!!")
+    }
+}
+
+
+app.use(checkAuth)
+
+app.get('/users/:id', function(req, res){
+
+    const id = req.params.id;
+    console.log(`<h1>Estamos buscando Usuário pelo ID: ${id}</h1>`)
+    res.sendFile(`${basePath}/users.html`)
+
+})
 
 app.get('/', (req, res) => {
 
